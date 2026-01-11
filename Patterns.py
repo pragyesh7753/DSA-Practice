44c1253a5731fee41295752b2044ac3e7bb8192a
Pragyesh Kumar Seth <116973800+pragyesh7753@users.noreply.github.com>
2026-01-11 21:34:23 +0530

    2nd day

--- Patterns.py
+++ Patterns.py
@@ -8,3 +8,20 @@ for i in range(4):
         print("*", end="")
     print()
 '''
+
+'''
+*
+**
+***
+****
+*****
+
+num = int(input("Enter a number: "))
+def pattern(num):
+    for i in range(num + 1):
+        for j in range(i):
+            print("*", end="")
+        print()
+        
+pattern(num)
+'''
