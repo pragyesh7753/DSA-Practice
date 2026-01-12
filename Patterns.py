0bc4278a9fa9eb4a1cb9b4104b5b98efa24a9fdf
Pragyesh Kumar Seth <116973800+pragyesh7753@users.noreply.github.com>
2026-01-12 23:34:47 +0530

    12/01/2026

--- Patterns.py
+++ Patterns.py
@@ -55,3 +55,40 @@ def pattern(num):
 
 pattern(6)
 '''
+
+'''
+*****
+****
+***
+**
+*
+
+def pattern(num):
+    for i in range(1, num + 1):
+        for j in range(1, num - i + 2):
+            print("*", end ="")
+        print()
+'''
+
+'''
+12345
+1234
+123
+12
+1
+
+def pattern(num):
+    for i in range(1, num + 1):
+        for j in range(1, num - i + 2):
+            print(j, end = "")
+        print()
+'''        
+
+'''
+    *
+   ***
+  *****
+ *******
+*********
+
+'''
