ee8cce356206b22628ee5bbe4801eb5f78d8b651
Pragyesh Kumar Seth <116973800+pragyesh7753@users.noreply.github.com>
2026-01-13 00:13:09 +0530

    12/01/2026

--- Patterns.py
+++ Patterns.py
@@ -90,5 +90,11 @@ def pattern(num):
   *****
  *******
 *********
-
+def pattern(num):
+    for i in range(1, num + 1):
+        for j in range(num - i):
+            print(" ", end="")
+        for k in range(2 * i - 1):
+            print("*", end="")
+        print()
 '''
