bab26ecdc9d4c91b76ec46b57da22801965981b9
Pragyesh Kumar Seth <116973800+pragyesh7753@users.noreply.github.com>
2026-03-20 23:59:52 +0530

    20/03/2026

--- rotation_of_array.py
+++ rotation_of_array.py
@@ -1,24 +1,52 @@
 # Right rotation of array by k steps
-def rightRotate(l1, k):
-    temp = [0] * len(l1)
-    for i in range(len(l1)):
-        newIndex = (i + k) % len(l1)
-        temp[newIndex] = l1[i]
-    print(temp)
+# def rightRotate(arr, k):
+#     temp = [0] * len(arr)
+#     for i in range(len(arr)):
+#         newIndex = (i + k) % len(arr)
+#         temp[newIndex] = arr[i]
+#     print(temp)
 
 
-rightRotate([1, 2, 3, 4, 5, 6], 4)
 # Time Complexity: O(n)
 # Space Complexity: O(n)
 
 
 # Left rotation of array by k steps
-def leftRotate(l1, k):
-    temp = [0] * len(l1)
-    for i in range(len(l1)):
-        newIndex = (i - k) % len(l1)
-        temp[newIndex] = l1[i]
-    print(temp)
+# def leftRotate(arr, k):
+#     temp = [0] * len(arr)
+#     for i in range(len(arr)):
+#         newIndex = (i - k) % len(arr)
+#         temp[newIndex] = arr[i]
+#     print(temp)
 
 
-leftRotate([1, 2, 3, 4, 5, 6], 3)
+
+# Right rotate without using a new array
+def swap(arr, start, end):
+    while start < end:
+        arr[start], arr[end] = arr[end], arr[start]
+        start += 1
+        end -= 1
+
+
+def rightRotate(arr, k):
+    n = len(arr)
+    k = k % n
+    swap(arr, 0, n - 1)
+    swap(arr, 0, k - 1)
+    swap(arr, k, n - 1)
+    print(arr)
+
+
+rightRotate([1, 2, 3, 4, 5, 6], 2)
+
+
+def leftRotate(arr, k):
+    n = len(arr)
+    k = k % n
+    swap(arr, 0, k - 1)
+    swap(arr, k, n - 1)
+    swap(arr, 0, n - 1)
+    print(arr)
+
+leftRotate([1, 2, 3, 4, 5, 6], 2)
